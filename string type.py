9cfece6e16697b820f989a636992878ce39d555b
Jiku Ahmed <155997238+Mdshazzadhossainjiku@users.noreply.github.com>
2025-08-19 00:02:07 +0600

    string type
    string type

---
+++ string type.py
@@ -0,0 +1,6 @@
+#In Python, a variable name cannot start with 1, 2, or 3 (any number). But numbers can be placed after the variable name.
+# 1name = "Jiku"   # ❌ This will give an error  
+# name1 = "Jiku"   # ✅ This works
+
+Name_1 ="JIKU Ahmed 73" 
+print("Name_1")
