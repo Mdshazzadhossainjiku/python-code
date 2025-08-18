36bf8af72f3538a9210c2a42d09e551cafdb94b0
Jiku Ahmed <155997238+Mdshazzadhossainjiku@users.noreply.github.com>
2025-08-18 11:11:36 +0600

    python variable
    python variable declare

---
+++ python variable.py
@@ -0,0 +1,3 @@
+#In Python, we don’t use single or double quotation marks when declaring a variable. If we do, Python will treat it as a string and show exactly that in the output. So, we should not use single or double quotation marks when declaring variables
+name ="Jiku Ahmed"
+print(name)
