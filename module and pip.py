498f924d5862db7983ffb678b9cd6ef0101cafd7
Jiku Ahmed <155997238+Mdshazzadhossainjiku@users.noreply.github.com>
2025-08-18 10:58:07 +0600

    module and pip
    module and pip

---
+++ module and pip.py
@@ -0,0 +1,3 @@
+#A Python module is a file containing Python code. It serves as a fundamental unit for organizing and reusing code in Python. Modules can define functions, classes, and variables, and can also include runnable code. 
+#pip is the standard package manager for Python.
+print("Hello world")
